8013f7a7ce788da3161acca26631984c69e91a0e
imcinerney <ian@simondata.com>
2019-09-17 17:59:57 -0400

    Increased limit

--- tap_sendgrid/syncs.py
+++ tap_sendgrid/syncs.py
@@ -124,7 +124,7 @@ class Syncer(object):
     def get_members_limits(self, stream, url_key=None):
         """Grabs all members incremental for a given stream"""
         offset = 0
-        limit = 5000
+        limit = 50000
         endpoint = stream.endpoint.format(url_key) if url_key else stream.endpoint
 
         while True:
