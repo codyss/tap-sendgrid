3696fefd6ac882066dbf70975f8cc48b310bb768
asher <asher@simondata.com>
2021-02-15 13:37:29 -0500

    add custom sg headers

--- tap_sendgrid/http.py
+++ tap_sendgrid/http.py
@@ -7,9 +7,14 @@ from singer import metrics
 session = requests.Session()
 logger = singer.get_logger()
 
+# see https://app.asana.com/0/1189361572142836/1194396938962481/f and 
+# https://simondata.atlassian.net/browse/DI-259?atlOrigin=eyJpIjoiOTJhM2FkNzVjODEzNDIwYjk2Y2JlOTA4YjQ4ZWRlY2QiLCJwIjoiaiJ9
+CUSTOM_HEADERS = {'X-SGUID': '3459112'}
+
 
 def authed_get(tap_stream_id, url, config, params=None):
     headers = {"Authorization": "Bearer %s" % config['api_key']}
+    headers.extend(CUSTOM_HEADERS)
     with metrics.http_request_timer(tap_stream_id) as timer:
         resp = session.request(method='get', url=url, params=params, headers=headers)
         timer.tags[metrics.Tag.http_status_code] = resp.status_code
